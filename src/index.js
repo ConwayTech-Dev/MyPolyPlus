b306a52b67087b58c300b04dcfc965e4e9bc7f7d
Colin Kim <colinvkim@gmail.com>
2024-02-21 18:32:59 -0800

    Preserve window state for a more native experience

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+import { StatefullBrowserWindow } from 'stateful-electron-window';
+
+var cmd = process.argv[1];
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
-const createWindow = () => {
+const createFirstRunWindow = () => {
   // Create the browser window.
-  const win = new BrowserWindow({
+  const win = new StatefullBrowserWindow({
     show: false,
     titleBarStyle: 'hidden',
     icon: path.join(__dirname, '/assets/icon.png'),
     webPreferences: {
+      nodeIntegration: true,
       preload: path.join(__dirname, 'preload.js'),
     },
   });
@@ -32,13 +36,44 @@ const createWindow = () => {
     splash.close();
     win.show();
     win.maximize();
+  }, 3900);
+};
+
+const createNormalWindow = () => {
+  // Create the browser window.
+  const win = new StatefullBrowserWindow({
+    show: false,
+    titleBarStyle: 'hidden',
+    icon: path.join(__dirname, '/assets/icon.png'),
+    webPreferences: {
+      nodeIntegration: true,
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+  win.loadURL("https://app.blackbaud.com/signin?redirectUrl=https%3A%2F%2Fpolytechnic.myschoolapp.com%2Fapp");
+  
+  var splash = new BrowserWindow({
+    width: 500, height: 300,
+    transparent: true,
+    frame: false,
+    alwaysOnTop: true
+  });
+
+  splash.loadFile('src/splash/index.html');
+  splash.center();
+
+  setTimeout(function() {
+    splash.close();
+    win.show();
   }, 4200);
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+if (cmd == '--squirrel-firstrun') {
+  // Running for the first time.
+  app.on('ready', createFirstRunWindow);
+} else {
+  app.on('ready', createNormalWindow);
+}
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
