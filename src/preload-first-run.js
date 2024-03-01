b6cc88a03224cdf1eb0309b9da7bed665eb9b48e
Colin Kim <colinvkim@gmail.com>
2024-02-29 23:16:18 -0800

    Finally fix CSS!

--- src/preload-first-run.js
+++ src/preload-first-run.js
@@ -1,3 +1,20 @@
+window.addEventListener('DOMContentLoaded', async () => {
+  // manipulate DOM here
+  
+  // adding css
+  const head = document.getElementsByTagName('head')[0];
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.type = 'text/css';
+  link.integrity = 'sha384-n6VRASnL5lEZSf0HAo0yiUqBQU22vDvstla2A6dId3mkUPHbtrpAd74A1qslW+uZ'; // Spoof integrity
+  link.href = 'https://conwaytech-dev.github.io/MyPolyPlus/src/index.css';
+  link.crossOrigin = 'anonymous';
+  head.appendChild(link);
+  
+  // removing the current css
+  // here you can do whatever you want
+  });
+
 // Fixing Blackbaud's ugly login screen
 document.getElementsByClassName("intro-paragraph").remove();
 document.getElementsByClassName("spa-auth-row").remove();
@@ -5,32 +22,6 @@ document.getElementsByClassName("spa-auth-link").remove();
 document.getElementsByClassName("spa-auth-full").remove();
 document.getElementsByClassName("center").remove();
 document.getElementsByClassName("sky-field-label").remove();
-
-window.addEventListener('DOMContentLoaded', () => {
-    const head = document.getElementsByTagName('head')[0];
-    const renderer = document.createElement('script');
-    renderer.src = './src/renderer.js';
-    renderer.integrity = 'sha384-TFHQ2jjldnEXG6p4acHgAFLMc2UFrK8wxiDKLT6JmJU7fdX8X8zXqEhV8ZJ1n4iu'; // Spoof integrity
-    head.appendChild(renderer);
-  });
-
-window.addEventListener('DOMContentLoaded', async () => {
-  // manipulate DOM here
-  
-  // adding css
-  const head = document.getElementsByTagName('head')[0];
-  const link = document.createElement('link');
-  link.id = ss;
-  link.rel = 'stylesheet';
-  link.type = 'text/css';
-  link.integrity = 'sha384-I/fNTK/rdiSJEnP3kccNgkwRIZNNS78pMw8MEv71Qf7dexnTgBRx+09/YUNkTmMP'; // Spoof integrity
-  link.href = 'https://raw.githubusercontent.com/ConwayTech-Dev/MyPolyPlus/main/src/index.css';
-  link.crossOrigin = 'anonymous';
-  head.appendChild(link);
-  
-  // removing the current css
-  // here you can do whatever you want
-  });
 
 window.addEventListener('DOMContentLoaded', () => {
     // Fixing the bad Poly logo
